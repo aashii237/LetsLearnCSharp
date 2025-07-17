cbe330e5a37174c0c29ee1eabcb663fe8c63afb9
Aasawari Pande <62434687+aashii237@users.noreply.github.com>
2025-07-17 17:22:15 +0000

    test

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -6,11 +6,11 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        //Second Obj = new Second();
-        //Obj.Method();
+        Second Obj = new Second();
+        Obj.Method();
 
-        //Third Obj3 = new Third();
-        //Obj3.Months();
+        Third Obj3 = new Third();
+        Obj3.Months();
 
         Forth Obj4 = new Forth();
         Obj4.Numbers();
